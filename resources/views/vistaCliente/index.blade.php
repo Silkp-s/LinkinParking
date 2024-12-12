e587a75668f25c6179d51add4a46ee3a480243e3
Faundez21 <cfaundezs@it.ucsc.cl>
2024-12-12 15:09:19 -0300

    css a los modals

--- resources/views/vistaCliente/index.blade.php
+++ resources/views/vistaCliente/index.blade.php
@@ -104,7 +104,7 @@
   <!-- Modal Pagarr -->
 <div class="modal fade" id="liberar" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
+    <div class="modal-content" style="background-color: #BBDEFB; color: black;">
       <div class="modal-header">
         <h5 class="modal-title" id="pagoModalLabel">Centro de Pago</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
--- resources/views/vistaEmpleado/index.blade.php
+++ resources/views/vistaEmpleado/index.blade.php
@@ -87,7 +87,7 @@
               <!-- Modal reservar -->
 <div class="modal fade" id="agendar" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
-      <div class="modal-content">
+    <div class="modal-content" style="background-color: #BBDEFB; color: black;">
         <div class="modal-header">
           <h1 class="modal-title fs-5" id="exampleModalLabel">Hola!</h1>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
@@ -146,7 +146,7 @@
   <!-- Modal liberar -->
 <div class="modal fade" id="liberar" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
-      <div class="modal-content">
+      <div class="modal-content" style="background-color: #BBDEFB; color: black;">
         <div class="modal-header">
           <h1 class="modal-title fs-5" id="exampleModalLabel">Pago Reservacion</h1>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
